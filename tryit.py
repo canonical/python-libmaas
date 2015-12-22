c181d6866a2a79a59d0aebc844026715eaadc7eb
Gavin Panella <gavin@gromper.net>
2015-12-22 15:21:59 +0000

    Drop compatibility with Python 2.

--- tryit.py
+++ tryit.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2.7
+#!/usr/bin/env python3.5
 # Copyright 2015 Alburnum Ltd. This software is licensed under
 # the GNU Affero General Public License version 3 (see LICENSE).
 
