41aeae185885665b4a88c21e18e28f7a8f115294
Gavin Panella <gavin@gromper.net>
2016-01-11 00:16:38 +0000

    New command, launch-node.

--- alburnum/maas/flesh/__init__.py
+++ alburnum/maas/flesh/__init__.py
@@ -336,6 +336,7 @@ class cmd_list_users(OriginTableCommand):
 
 
 class cmd_acquire_node(OriginTableCommand):
+    """Acquire a node."""
 
     def __init__(self, parser):
         super(cmd_acquire_node, self).__init__(parser)
@@ -345,16 +346,54 @@ class cmd_acquire_node(OriginTableCommand):
         parser.add_argument("--memory", type=float)
         parser.add_argument("--tags", default="")
 
-    def execute(self, origin, options, target):
-        node = origin.Nodes.acquire(
+    def acquire(self, origin, options):
+        return origin.Nodes.acquire(
             hostname=options.hostname, architecture=options.architecture,
             cpus=options.cpus, memory=options.memory,
             tags=options.tags.split())
+
+    def execute(self, origin, options, target):
+        node = self.acquire(origin, options)
         table = tables.NodesTable()
         print(table.render(target, [node]))
 
 
+class cmd_launch_node(cmd_acquire_node):
+    """Acquire and deploy a node."""
+
+    def __init__(self, parser):
+        super(cmd_launch_node, self).__init__(parser)
+        parser.add_argument(
+            "--wait", type=int, default=0, help=(
+                "Number of seconds to wait for deploy to complete."))
+
+    def execute(self, origin, options, target):
+        node = self.acquire(origin, options)
+        table = tables.NodesTable()
+
+        print(colorized("{automagenta}DEPLOYING:{/automagenta}"))
+        print(table.render(target, [node]))
+
+        with utils.Spinner():
+            node = node.start()
+            for elapsed, remaining, wait in utils.retries(options.wait, 1.0):
+                if node.substatus_name == "Deploying":
+                    sleep(wait)
+                    node = origin.Node.read(system_id=node.system_id)
+                else:
+                    break
+
+        if node.substatus_name == "Deployed":
+            print(colorized("{autogreen}DEPLOYED:{/autogreen}"))
+            print(table.render(target, [node]))
+        else:
+            print(colorized("{autored}FAILED TO DEPLOY:{/autored}"))
+            print(table.render(target, [node]))
+            raise CommandError("Node was not deployed.")
+
+
 class cmd_release_node(OriginTableCommand):
+    """Release a node."""
 
     def __init__(self, parser):
         super(cmd_release_node, self).__init__(parser)
@@ -369,11 +408,11 @@ class cmd_release_node(OriginTableCommand):
 
         with utils.Spinner():
             for elapsed, remaining, wait in utils.retries(options.wait, 1.0):
-                if node.substatus_name == "Ready":
-                    break
-                else:
+                if node.substatus_name == "Releasing":
                     sleep(wait)
-                    node = node.release()
+                    node = origin.Node.read(system_id=node.system_id)
+                else:
+                    break
 
         table = tables.NodesTable()
         print(table.render(target, [node]))
@@ -402,6 +441,7 @@ def prepare_parser(argv):
 
     # Node lifecycle.
     cmd_acquire_node.register(parser)
+    cmd_launch_node.register(parser)
     cmd_release_node.register(parser)
 
     parser.add_argument(
