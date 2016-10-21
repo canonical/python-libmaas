f8568dc4bd34e5a0b137ddd8ecc52bb70e975e3b
Brendan Donegan <brendan.j.donegan@gmail.com>
2016-10-21 13:55:55 +0100

    add docstring to bind

--- alburnum/maas/viscera/testing.py
+++ alburnum/maas/viscera/testing.py
@@ -6,6 +6,10 @@ from unittest.mock import MagicMock
 
 
 def bind(cls, origin=None, handler=None):
+    """
+    Returns a new version of the class that binds the origin
+    and handler of that class with MagicMock (unless you specify otherwise)
+    """
     return cls.bind(
         (MagicMock() if origin is None else origin),
         (MagicMock() if handler is None else handler),
