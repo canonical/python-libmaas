7198133cf9d24f3d29d300366951b7eac8b2547f
Blake Rouse <blake.rouse@vindeka.com>
2016-10-24 15:49:36 -0400

    Change to is_superuser and make email optional.

--- alburnum/maas/viscera/users.py
+++ alburnum/maas/viscera/users.py
@@ -20,7 +20,9 @@ class UsersType(ObjectType):
     def __iter__(cls):
         return map(cls._object, cls._handler.read())
 
-    def create(cls, username, email, password, *, is_superuser=False):
+    def create(cls, username, password, *, email=None, is_superuser=False):
+        if email is None:
+            email = "%s@null.maas.io" % username
         data = cls._handler.create(
             username=username, email=email, password=password,
             is_superuser='1' if is_superuser else '0')
@@ -42,5 +44,5 @@ class User(Object):
         "username", check(str), check(str))
     email = ObjectField.Checked(
         "email", check(str), check(str))
-    is_admin = ObjectField.Checked(
+    is_superuser = ObjectField.Checked(
         "is_superuser", check(bool), check(bool))
