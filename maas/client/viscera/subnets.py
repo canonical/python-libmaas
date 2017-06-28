93d6aa2415031021c0d9f216bebc4852b1a08200
Greg Lutostanski <gregory.lutostanski@canonical.com>
2017-06-28 11:30:44 -0500

    subnets: include other create options

--- maas/client/viscera/subnets.py
+++ maas/client/viscera/subnets.py
@@ -5,6 +5,8 @@ __all__ = [
     "Subnet",
 ]
 
+from typing import Union
+
 from . import (
     check,
     check_optional,
@@ -14,6 +16,9 @@ from . import (
     ObjectSet,
     ObjectType,
 )
+from .fabrics import Fabric
+from .space import Space
+from .vlan import Vlan
 
 
 class SubnetsType(ObjectType):
@@ -23,8 +28,12 @@ class SubnetsType(ObjectType):
         data = await cls._handler.read()
         return cls(map(cls._object, data))
 
-    async def create(cls, *, cidr: str, name: str=None,
-                     description: str=None):
+    async def create(cls, *, cidr: str, name: str=None, description: str=None,
+                     fabric: Union[Fabric, int]=None,
+                     vlan: Union[Vlan, int]=None,
+                     vid: int=None, space: Union[Space, int]=None,
+                     gateway_ip: str=None, rdns_mode: str=None,
+                     dns_servers: str=None):
         """
         Create a `Subnet` in MAAS.
 
@@ -41,6 +50,40 @@ class SubnetsType(ObjectType):
         params = {
             "cidr": cidr
         }
+        if isinstance(fabric, int):
+            params["fabric"] = fabric
+        elif isinstance(fabric, Fabric):
+            params["fabric"] = fabric.id
+        else:
+            raise TypeError(
+                "fabric must be Fabric or int, not %s" % (
+                    type(fabric).__class__))
+        if isinstance(vlan, int):
+            params["vlan"] = vlan
+        elif isinstance(vlan, vlan):
+            params["vlan"] = vlan.id
+        else:
+            raise TypeError(
+                "vlan must be Vlan or int, not %s" % (
+                    type(vlan).__class__))
+        if isinstance(space, int):
+            params["space"] = space
+        elif isinstance(space, space):
+            params["space"] = space.id
+        else:
+            raise TypeError(
+                "space must be space or int, not %s" % (
+                    type(space).__class__))
+
+        if vid is not None:
+            assert(vlan is None)
+            params["vid"] = vid
+        if gateway_ip is not None:
+            params["gateway_ip"] = gateway_ip
+        if rdns_mode is not None:
+            params["rdns_mode"] = rdns_mode
+        if dns_servers is not None:
+            params["dns_servers"] = dns_servers
         if name is not None:
             params["name"] = name
         if description is not None:
@@ -72,15 +115,12 @@ class Subnet(Object, metaclass=SubnetType):
     description is allowed in the create call and displayed in the UI
     but never returned by the API
     """
-    vlan = ObjectFieldRelated(
-        "vlan", "Vlan")
-    fabric = ObjectFieldRelated(
-        "fabric", "Fabric")
+    vlan = ObjectFieldRelated("vlan", "Vlan")
+    fabric = ObjectFieldRelated("fabric", "Fabric")
     name = ObjectField.Checked(
         "vid", check_optional(int)
     )
-    space = ObjectFieldRelated(
-        "space", "Space")
+    space = ObjectFieldRelated("space", "Space")
     gateway_ip = ObjectField.Checked(
         "gateway_ip", check(str),
     )
