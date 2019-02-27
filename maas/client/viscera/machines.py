4092c68ef7fb1753efc843569848e2bcc3415002
Juan Vela <juan.vela.zgz@gmail.com>
2019-02-27 19:05:31 +0100

    Fix power_parameters JSON parsing error (#206)
    * Fix power_parameters JSON parsing error
    
    * Fix test
    
    * Fix test typo
    
    * Fix lint error (line too long)
    
    * Try to fix lint error (continuation line under-indented)
    
    * Sort keys when parsing to JSON in order to be able to predict the result and pass the tests
    
    * Fix lint error (line too long)

--- maas/client/viscera/machines.py
+++ maas/client/viscera/machines.py
@@ -8,6 +8,7 @@ __all__ = [
 import asyncio
 import base64
 import bson
+import json
 from http import HTTPStatus
 import typing
 
@@ -99,7 +100,8 @@ class MachinesType(NodesType):
             "power_type": power_type,
         }
         if power_parameters is not None:
-            params["power_parameters"] = power_parameters
+            params["power_parameters"] = json.dumps(
+                power_parameters, sort_keys=True)
         if subarchitecture is not None:
             params["subarchitecture"] = subarchitecture
         if min_hwe_kernel is not None:
