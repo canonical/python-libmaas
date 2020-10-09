575d3f762afc69ae91349aa75a893a53826d6f22
Adam Collard <adam.collard@canonical.com>
2020-10-09 16:45:24 +0100

    Release 0.6.6
    Signed-off-by: Adam Collard <adam.collard@canonical.com>

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     author='MAAS Developers',
     author_email='maas-devel@lists.launchpad.net',
     url='https://github.com/maas/python-libmaas',
-    version="0.6.5",
+    version="0.6.6",
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
